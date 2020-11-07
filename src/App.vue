f9198d9f18c20d1b081cb60d6246d15c3ff55c0d
David Franko <david.franko1998@gmail.com>
2020-11-08 00:49:12 +0100

    Added start script to package json

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <v-app>
-    <v-app-bar color="orange" app>
-      <div >
-        <h1>Will you show logo pls</h1>
-        <v-img src="assets/logo.svg" class="image"/>
-      </div>
+    <v-app-bar app elevation="1">
+      <v-spacer></v-spacer>
+      <img class="logo" src="@/assets/logo.png" alt="Logo"/>
+      <v-spacer></v-spacer>
     </v-app-bar>
     <v-main>
       <v-container fluid>
@@ -22,11 +21,9 @@ export default {
 </script>
 
 <style>
-  .image{
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    width: 5%;
-    height: auto;
-  }
+.logo {
+  height: 80%;
+  width: auto;
+}
+
 </style>
